310cebc24d345ebd927c3dc069e7473cadde7be8
Liam <liam.ladrouz@gmail.com>
2019-04-05 17:23:25 +0200

    Liaison Front - Back avec axios, le port utilisé pour le front est le 8080, pour le back c'est le 8000.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  data () {
+    return {
+      ville: {
+        nom: 'roubaix'
+      }
+    }
+  },
   router,
   components: { App },
+  mounted () {
+    axios
+      .get('http://localhost:8000/meteo/' + this.ville + '/humidite')
+      .then(response => (this.info = response))
+  },
   template: '<App/>'
 })
