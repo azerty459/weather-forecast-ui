d83d71ecfa6d906d92131f18b007a76216b3fb9d
Arthur Brandao <abrandao@nextoo.fr>
2019-04-12 09:51:26 +0200

    Ajout message d'erreur*

--- src/components/Prevision.vue
+++ src/components/Prevision.vue
@@ -4,14 +4,20 @@
         <div v-if="city">
           <h3>Pr Vive {{city}}</h3>
         </div>
+        <div v-if="messageError">
+          <error :message="messageError"/>
+        </div>
     </div>
 </template>
 
 <script>
+import error from '@/components/message/Error.vue'
+
 export default{
   data () {
     return {
-      city: null
+      city: null,
+      messageError: null
     }
   },
   props: ['ville'],
@@ -20,6 +26,9 @@ export default{
     window.bus.$on('newSearch', (ville) => {
       this.city = ville
     })
+  },
+  components: {
+    error
   }
 }
 </script>
---
+++ src/components/message/Error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="hide-on-small-only">
+      <div class="row red-text red lighten-4" style="height: 60px; border: dashed;">
+        <div class="col s2">
+          <i class="material-icons" style="font-size: 3em; line-height: 125%">error</i>
+        </div>
+        <div class="col s10 truncate " style="font-size: 1.5em; line-height: 250%">
+          {{message}}
+        </div>
+      </div>
+    </div>
+    <div class="hide-on-med-and-up">
+      <div class="red lighten-4 red-text center" style="border: dashed">
+        <br>{{message}}<br>&nbsp;
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  props: ['message']
+}
+</script>
+
+<style>
+
+</style>
